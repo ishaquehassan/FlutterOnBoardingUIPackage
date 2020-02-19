410506dc138e6a93067df71ff222f2e12e1fd34f
Ishaq Hassan <ishaquehassan@gmail.com>
2020-02-19 18:55:56 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# flutter_onboarding_ui
+
+A conceptual design for on boarding screens for mobile apps. This app provides you with all the information you need to explore how to work with mobile on boarding in mobile apps.
+
+![preview (1)](https://user-images.githubusercontent.com/48721096/71137132-1d0a5f80-2218-11ea-9086-a241341baf72.png)
+
+Video Preview: http://bit.ly/2SF79c6
+
+Live Coding Video: http://bit.ly/2QgrZgg
+
+Android Demo APK: http://bit.ly/2Q6eD67
+
+Just incase you liked this then you might want to check this one out: http://bit.ly/2SziCts
---
+++ lib/screens/landing_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../ui_view/slider_layout_view.dart';
+
+class LandingPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: onBordingBody(),
+    );
+  }
+
+  Widget onBordingBody() => Container(
+        child: SliderLayoutView(),
+      );
+}
